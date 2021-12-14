790a39a9360bac7ed0c9d353df3441ecf991c00a
FedericoGerminario <fedege98@gmail.com>
2021-12-14 18:51:41 +0100

    Finished first classification

--- main.py
+++ main.py
@@ -7,24 +7,12 @@ from utility import *
 def main():
     train_input_path = "train/patched_images/"
     trained_model_path ="model"
-    '''
-    gpus = tf.config.list_physical_devices('GPU')
-    if gpus:
-        # Restrict TensorFlow to only allocate 1GB of memory on the first GPU
-        try:
-            tf.config.set_logical_device_configuration(
-                gpus[0],
-                [tf.config.LogicalDeviceConfiguration(memory_limit=1024)])
-            logical_gpus = tf.config.list_logical_devices('GPU')
-            print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
-        except RuntimeError as e:
-            # Virtual devices must be set before GPUs have been initialized
-            print(e)
+    trained_model_path_active = "modelActive"
+    results_whole_dataset = "./train/model/results.pkl"
+    results_active = "./train/model/resultsActive.pkl"
 
-    '''
-
-    train_whole_dataset(train_input_path, trained_model_path, "./train/model/results.pkl")
-
+    #train_whole_dataset(train_input_path, trained_model_path, results_whole_dataset)
+    teach_model(train_input_path, trained_model_path_active, results_active, num_iteration=10)
 
     print("End")
 
