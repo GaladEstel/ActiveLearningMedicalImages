d3d807f6bc29366647d54142be87b7993fe3c111
GabrieleGioetto <gabriele.gioetto@gmail.com>
2021-12-15 17:12:34 +0100

    fixed main merge in master

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from utility import *
 
 def main():
     train_input_path = "train/patched_images/"
-<<<<<<< HEAD
     trained_model_path = "model"
     train_metadata_filepath = "./train/model"
     '''
@@ -25,18 +24,16 @@ def main():
 
     '''
 
-    train_whole_dataset(train_input_path, trained_model_path, train_metadata_filepath)
-=======
-    trained_model_path ="model"
-    trained_model_path_active = "modelActive"
-    results_whole_dataset = "./train/model/results.pkl"
-    results_active = "./train/model/resultsActive.pkl"
->>>>>>> 0e8e304faacb33f81f96930fc984e1541ef9bf8b
 
-    #train_whole_dataset(train_input_path, trained_model_path, results_whole_dataset)
+    # train_whole_dataset(train_input_path, trained_model_path, train_metadata_filepath)
+
+    trained_model_path_active = "modelActive"
+    results_active = "./train/model/resultsActive.pkl"
+
     teach_model(train_input_path, trained_model_path_active, results_active, num_iteration=10)
 
     print("End")
+
 
 if __name__ == "__main__":
     main()
